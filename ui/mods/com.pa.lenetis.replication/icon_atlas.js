d02efefea42169b31be694923ef401a1cb719dbd
Lenetis <70149832+Lenetis@users.noreply.github.com>
2021-07-19 13:37:14 +0200

    Add files via upload

--- ui/mods/com.pa.lenetis.replication/icon_atlas.js
+++ ui/mods/com.pa.lenetis.replication/icon_atlas.js
@@ -1,4 +1,5 @@
 var paeiouIcons = [
-    "orbital_fabrication_bot_adv"
+    "orbital_fabrication_bot_adv",
+	"l_orbital_fabrication_bot_adv"
 ];
 model.strategicIcons(model.strategicIcons().concat(paeiouIcons));
--- ui/mods/com.pa.lenetis.replication/shared_build.js
+++ ui/mods/com.pa.lenetis.replication/shared_build.js
@@ -1,5 +1,6 @@
 var newBuild = {
-	"/pa/units/orbital/orbital_fabrication_bot_adv/orbital_fabrication_bot_adv.json": ["orbital", 0, { row: 0, column: 4 }]
+	"/pa/units/orbital/orbital_fabrication_bot_adv/orbital_fabrication_bot_adv.json": ["orbital", 0, { row: 0, column: 4 }],
+	"/pa/units/orbital/l_orbital_fabrication_bot_adv/l_orbital_fabrication_bot_adv.json": ["L_orbital", 0, { row: 0, column: 4 }]
 }
 if (Build && Build.HotkeyModel && Build.HotkeyModel.SpecIdToGridMap) {
     _.extend(Build.HotkeyModel.SpecIdToGridMap, newBuild);
